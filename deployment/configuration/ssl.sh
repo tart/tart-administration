3c777552d72a4c1d9ff4745932a50ce78262c60d
Emre Hasegeli <hasegeli@startup.kitchen>
2014-07-31 20:00:57 +0300

    Do not use __global for SSL certificates.

--- deployment/configuration/ssl.sh
+++ deployment/configuration/ssl.sh
@@ -2,8 +2,6 @@
 
 function copy {
     ssh $2 test -d $3 || return 0
-    test -d $1/../__global && scp -q $1/../__global/* $2:$3
-    test -f $1/.gitkeep || scp -q $1/* $2:$3
     ssh $2 chgrp $4 $3/*
     ssh $2 chmod o-r $3/*
 }
